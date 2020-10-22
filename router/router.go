ca0becd92f7fb8b74c7d8f88122d2021ee027fbc
higashi000 <higashi.ict@gmail.com>
2020-10-22 23:21:39 +0900

    Add cors

--- router/router.go
+++ router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/nitoc-ict/fushigidane-server/convertaddress"
@@ -13,6 +15,10 @@ func NewRouter() *echo.Echo {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+    e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+        AllowOrigins: []string{"*"},
+        AllowMethods: []string{http.MethodGet, http.MethodPost},
+    }))
 
 	e.GET("/coordinate", convertaddress.ConvertAddress)
 	e.GET("/route", gettransitpoints.GetTransitPoints)
